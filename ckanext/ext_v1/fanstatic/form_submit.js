a152b6426995f61f3e997b07b36120691c90398b
Luís Santos <lsantos@ubiwhere.com>
2020-09-16 16:03:36 +0100

    quest_data_structure_view

--- ckanext/ext_v1/fanstatic/form_submit.js
+++ ckanext/ext_v1/fanstatic/form_submit.js
@@ -20,7 +20,7 @@ ckan.module('form_submit', function ($) {
                 }
             });
             //Function to create Dataset with CKAN API
-            function create_dataset_and_store_resource(dataset_name, organization_id, type_quest, final_json_to_send) {
+            function create_dataset_and_store_resource(dataset_name, organization_id, type_quest, final_json_to_send, tmp_obj) {
                 //Create data form to send in the request
                 var dataset_id = dataset_name.split(" ").join("-").toLowerCase();
                 let json_data = {};
@@ -42,7 +42,7 @@ ckan.module('form_submit', function ($) {
                     success: function (data) {
                         console.log(data.result);
                         //Create new resource and insert into it
-                        create_and_insert_first_row_into_resource(dataset_id, type_quest, final_json_to_send);
+                        create_and_insert_first_row_into_resource(dataset_id, type_quest, final_json_to_send, tmp_obj);
                     },
                     error: function (ts) {
                         console.log(ts.responseText);
@@ -52,7 +52,10 @@ ckan.module('form_submit', function ($) {
             }
 
             //Function to insert table into resource and first row with Datastore API
-            function create_and_insert_first_row_into_resource(dataset_name, resource_name, final_json_to_send) {
+            function create_and_insert_first_row_into_resource(dataset_name, resource_name, final_json_to_send, tmp_obj) {
+                //Get types and consequent questions/answer keys
+                var fiels_resource = [{ "id": "id" }, { "id": "info" }, { "id": "date_submitted" }];
+                tmp_obj.map((x => { fiels_resource.push({ "id": x }) }));
                 //ajax request to create resource
                 $.ajax({
                     url: url + 'api/3/action/datastore_create', // create resource path
@@ -64,9 +67,10 @@ ckan.module('form_submit', function ($) {
                         "resource": {
                             "package_id": dataset_name,
                             "name": resource_name,
-                            "format": "json"
+                            "format": "csv"
                         },
                         "force": true,
+                        "fields": fiels_resource,
                         "records": [final_json_to_send]
                     }),
                     success: function (data) {
@@ -82,6 +86,7 @@ ckan.module('form_submit', function ($) {
             //Function to insert data row into resource with Datastore API
             function insert_new_row_into_resource(resource_id, final_json_to_send) {
                 //ajax request to insert data row into resource
+                console.log(final_json_to_send["records"]);
                 $.ajax({
                     url: url + 'api/3/action/datastore_upsert', //insert row in resource path
                     type: 'POST',
@@ -111,43 +116,48 @@ ckan.module('form_submit', function ($) {
                 $("#finish_quest").css("display", "none");
                 // Show loader
                 $("#loader").css("display", "block");
-                var overall_obj = {}
+                var overall_obj = [];
+                let questions_entity = [];
+                var tmp_obj = [];
                 //Get all info from questionnaire
                 $('#quest_content_form > div').each(function () {
                     var key = this.id;
                     var key_id = key.split("_")[0];
-                    var tmp_obj = [];
-                    let questions_entity = [];
+                    //let questions_entity = [];
                     // Get all checked values from questionnaire tables
                     $("#" + key + " #all_tables tbody tr").each(function () {
+                        // Get id row
+                        var key_opt = $(this).find('input[type="radio"]').attr("id");
+                        tmp_obj.push(key_opt + "_question");
+                        tmp_obj.push(key_opt + "_answer");
                         if ($(this).find('input[type="radio"]').is(":checked")) {
                             // Get question
                             var row_question = $(this).find('th').text().replace('\t', '').replace('*', '');
                             // Get answer
                             var row_opt = $(this).find('input[type="radio"]:checked').attr("value");
-                            // Get id row
-                            var key_opt = $(this).find('input[type="radio"]:checked').attr("id");
                             // Define data structure of questions/answers
-                            questions_entity.push({ 'id': key_opt, 'question': row_question, 'answer': row_opt });
+                            questions_entity.push({ [key_opt + "_question"]: row_question, [key_opt + "_answer"]: row_opt });
                         }
 
                     });
                     $("#" + key + " #all_tables .input_text").each(function () {
+                        // Get id row
+                        var key_opt = $(this).find('textarea').attr("id");
+                        tmp_obj.push(key_opt + "_question");
+                        tmp_obj.push(key_opt + "_answer");
                         if ($(this).find('textarea').val() != "") {
                             // Get question
                             var row_question = $(this).find('label').text().replace('\t', '').replace('*', '');
                             // Get answer
                             var row_opt = $(this).find('textarea').val();
-                            // Get id row
-                            var key_opt = $(this).find('textarea').attr("id");
                             // Define data structure of questions/answers
-                            questions_entity.push({ 'id': key_opt, 'question': row_question, 'answer': row_opt });
+                            questions_entity.push({ [key_opt + "_question"]: row_question, [key_opt + "_answer"]: row_opt });
                         }
                     });
-                    if (questions_entity.length > 0)
-                        tmp_obj.push({ 'entity': key, 'questions': questions_entity });
+                    // if (questions_entity.length > 0)
+                    //     tmp_obj.push(questions_entity);
 
-                    overall_obj[key_id] = tmp_obj;
+                    // overall_obj[key_id] = tmp_obj;
                 });
 
                 var resource_id = "";
@@ -204,16 +214,24 @@ ckan.module('form_submit', function ($) {
 
                             //Final json structure to send to rabbit
                             var currentdate = new Date();
-                            var final_json_to_send = { "id": type_quest, "info": type_quest + "_quest", "records": overall_obj, "date_submitted": currentdate };
+                            console.log(questions_entity[0]);
+                            var final_json_to_send = { "id": type_quest, "info": type_quest + "_quest", "date_submitted": currentdate };
+                            questions_entity.forEach(x => {
+                                for (const [key, value] of Object.entries(x)) {
+                                    final_json_to_send[key] = value
+                                }
+                            });
+                            console.log(final_json_to_send);
+                            //var final_json_to_send = { "id": type_quest, "info": type_quest + "_quest", "records": questions_entity, "date_submitted": currentdate };
                             // In case of data_store dataset doenst exists create them
                             if (!dataset_exists) {
                                 //Create dataset
                                 dataset_name = "Questionnaires Data from " + organization_name;
-                                create_dataset_and_store_resource(dataset_name, organization_id, type_quest, final_json_to_send);
+                                create_dataset_and_store_resource(dataset_name, organization_id, type_quest, final_json_to_send, tmp_obj);
                             }
                             else if (!resource_exists) {
                                 //Create new resource and insert into it
-                                create_and_insert_first_row_into_resource(dataset_name, type_quest, final_json_to_send);
+                                create_and_insert_first_row_into_resource(dataset_name, type_quest, final_json_to_send, tmp_obj);
                             }
                             else {
                                 //Insert into resource
