550c010fb2c9ce44b8ec3e23bdf968769757f3ef
Luís Santos <lsantos@ubiwhere.com>
2020-10-07 20:03:50 +0100

    Submit questionnaires and store them into resources without key

--- ckanext/ext_v1/plugin.py
+++ ckanext/ext_v1/plugin.py
@@ -1,9 +1,18 @@
+# CKAN imports
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
-from flask import Blueprint, request
 import ckan.lib.base as base
-import os
-import json, ast
+
+# Flask import
+from flask import Blueprint, request
+
+# Converters imports
+import ast
+import json
+
+# Collection import (order)
+from collections import OrderedDict
+
 
 # Blueprint
 questionnaire = Blueprint("questionnaire", __name__)
@@ -12,7 +21,7 @@ questionnaire = Blueprint("questionnaire", __name__)
 render = base.render
 
 
-# method to be able to render questionnaires on ckan
+# Method to be able to render questionnaires on ckan
 @questionnaire.route("/questionnaire", endpoint="custom_action")
 def custom_action():
     resource_id = request.args.get("resource-id", type=str)
@@ -44,36 +53,101 @@ def get_key(context, data_dict=None):
 @toolkit.side_effect_free
 def insert_quests(context, data_dict=None):
     """
-    Method to enable members to submit questionnaires and add them into a specific resource
+    Method to enable members to submit questionnaires and add them into a specific resource.
     By getting the dataset creator id, we are able to do a temporary login and get the apikey.
     With it, we can do datastore_update with success and store the response from any user.
     """
-    name_resource = data_dict["name_resource"]
+    # Get the name of the resource
+    name_resource = data_dict.pop("name_resource", None)
+    # Initialize variable to have direct access to dabatase
+    # Use this to do read requests only
     model = context["model"]
 
-    resource = (
-        model.Session.query(model.Resource)
-        .filter(model.Resource.name == name_resource)
-        .filter(model.Resource.state == "active")
-        .one()
-    )
-    if resource:
-        result = ast.literal_eval(json.dumps(data_dict["result"]))
-        data_to_send = {
-            "resource_id": resource.id.encode("utf-8"),
-            "force": "true",
-            "method": "insert",
-            "records": [ast.literal_eval(result)],
-        }
-        insert_quest = toolkit.get_action("datastore_upsert")(
-            context={"ignore_auth": "true"}, data_dict=data_to_send,
-        )
+    if name_resource:
 
-        return insert_quest
+        # Get the resource of the questionnaire submmitted
+        resource = (
+            model.Session.query(model.Resource)
+            .filter(model.Resource.name == name_resource)
+            .filter(model.Resource.state == "active")
+            .first()
+        )
+        result = ast.literal_eval(json.dumps(data_dict["result"]))
+        ordered_result = OrderedDict(
+            sorted(
+                sorted(
+                    ast.literal_eval(result).items(),
+                    key=lambda s: (
+                        s[0].split("_")[-1]
+                        if "_" in s[0] and len(s[0].split("_")) > 2
+                        else s[0]
+                    ),
+                    reverse=True,
+                ),
+                key=lambda s: (
+                    (
+                        s[0].split("_")[0]
+                        if "_" in s[0] and len(s[0].split("_")) > 2
+                        else s[0]
+                    ),
+                    (
+                        int(s[0].split("_")[-2])
+                        if "_" in s[0] and len(s[0].split("_")) > 2
+                        else s[0]
+                    ),
+                ),
+            ),
+        )
+        # If is exists
+        if resource:
+            data_to_send = {
+                "resource_id": resource.id.encode("utf-8"),
+                "force": "true",
+                "method": "insert",
+                "records": [ordered_result],
+            }
+            insert_quest = toolkit.get_action("datastore_upsert")(
+                context={"ignore_auth": "true"}, data_dict=data_to_send,
+            )
+
+            return insert_quest
+        else:
+            # Get dataset responsible for store quesitonnaires
+            dataset = (
+                model.Session.query(model.Package)
+                .join(model.PackageExtra)
+                .filter(model.Package.state == "active")
+                .filter(model.PackageExtra.key == "is_data_store")
+                .first()
+            )
+
+            # Create resource and insert on it
+            if dataset:
+                data_to_send = {
+                    "resource": {
+                        "package_id": dataset.name,
+                        "name": name_resource,
+                        "format": "json",
+                    },
+                    "force": "true",
+                    "method": "insert",
+                    "records": [ordered_result],
+                }
+                create_resource_and_insert_quest = toolkit.get_action(
+                    "datastore_create"
+                )(context={"ignore_auth": "true"}, data_dict=data_to_send,)
+
+                return create_resource_and_insert_quest
+            else:
+                return {
+                    "success": False,
+                    "msg": "Dataset name is invalid.",
+                }
     else:
-        # Create resource and insert on it
-        print("ww")
-    # VERIFY FIRST IF THERE IS DATASET
+        return {
+            "success": False,
+            "msg": "Resource name is invalid.",
+        }
 
 
 class Ext_V1Plugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
